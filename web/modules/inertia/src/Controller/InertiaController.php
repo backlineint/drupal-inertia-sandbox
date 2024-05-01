fe3dd7c9f7675909d6cd99acc35120e53aae68ca
Brian Perry <contact.brianperry@gmail.com>
2024-05-01 09:29:24 -0500

    Refactor test controller to handle any path

--- web/modules/inertia/src/Controller/InertiaController.php
+++ web/modules/inertia/src/Controller/InertiaController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Drupal\inertia\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Returns responses for Inertia routes.
@@ -14,17 +15,41 @@ final class InertiaController extends ControllerBase {
   /**
    * Builds the response.
    */
-  public function __invoke(): array {
+  public function __invoke($path): array {
+    $new_path = \Drupal::service('path_alias.manager')->getPathByAlias("/$path");
+
+    if(preg_match('/node\/(\d+)/', $new_path, $matches)) {
+      $node = \Drupal\node\Entity\Node::load($matches[1]);
+    }
+    else {
+      throw new NotFoundHttpException();
+    }
+
+    // Next - abstract constructing this and the build array to a set of helpers
+    $data_page = [
+      'component' => 'example',
+      'props' => [
+        'event' => [
+          'id' => $node->id(),
+          'title' => $node->getTitle(),
+          // Todo - test with HTML
+          'description' => $node->get('body')->value,
+        ],
+        // TODO - fix URL redirect
+        'url' => "/inertia/$path",
+        'version' => 'c32b8e4965f418ad16eaebba1d4e960f',
+      ],
+    ];
 
     $build['content'] = [
-      '#type' => 'item',
-      '#markup' => '<div id="app" data-page=\'{"component":"example","props":{"event":{"id":80,"title":"Birthday party","start_date":"2019-06-02","description":"Come out and celebrate Jonathan&apos;s 36th birthday party!"}},"url":"/events/80","version":"c32b8e4965f418ad16eaebba1d4e960f"}\'></div>
-      ',
+      '#type' => 'html_tag',
+      '#tag' => 'div',
+      '#attributes' => ['id' => 'app', 'data-page' => json_encode($data_page)],
       '#attached' => array(
         'library' => array(
             'inertia/app',
         ),
-    ),
+      ),
     ];
 
     return $build;
