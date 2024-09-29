8f2228eb9867b430b33210265f1741d587add432
Brian Perry <contact.brianperry@gmail.com>
2024-09-29 16:10:37 -0500

    Cleanup in examples module

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Inertia Sandbox
 
+Initial POCs for a Drupal implementation of [Inertia.js](https://inertiajs.com/)
+
 ## Setup
+
+TODO - Revise this section
 
 ddev start
 ddev composer install
@@ -10,4 +14,29 @@ ddev drush site:install -y
 
 ### Inertia
 
-Provides a custom route at `/inertia/example`
+TODO - Things to look at in inertia...
+
+### Inertia Examples
+
+Provides examples of the two existing methods provided by the Inertia module.
+
+#### Inertia::render
+
+The module provides a custom route at `/inertia/example` that uses the
+`Inertia::render` method to render a simple Inertia page.
+
+see `src/Controller/InertiaController.php` for more.
+
+#### Inertia::renderByContext
+
+In inertia_examples.module a preprocess_node hook calls the
+`Inertia::renderByContext` method to render render all nodes using
+Inertia.
+
+renderByContent will use the appropriate template in app/src/Pages based on the
+node template suggestion. All fields will be converted to slots which will be
+rendered by React.
+
+### How Inertia Examples uses Vite
+
+TODO
--- web/modules/custom/inertia_examples/src/Controller/InertiaController.php
+++ web/modules/custom/inertia_examples/src/Controller/InertiaController.php
@@ -16,29 +16,20 @@ final class InertiaController extends ControllerBase {
   /**
    * Builds the response.
    */
-  public function __invoke($path): array {
-    $new_path = \Drupal::service('path_alias.manager')->getPathByAlias("/$path");
-
-    if(preg_match('/node\/(\d+)/', $new_path, $matches)) {
-      $node = \Drupal\node\Entity\Node::load($matches[1]);
-    }
-    else {
-      throw new NotFoundHttpException();
-    }
-
-    // Typical calls for this should just be component, props
-    return Inertia::render('example', [
-      'node' => [
-        'id' => $node->id(),
-        'title' => $node->getTitle(),
-        'description' => $node->get('body')->value,
+  public function __invoke(): array {
+    /**
+     * A simple example of the Inertia render method.
+     * This will mount the app using the example.tsx component in /app/src/pages
+     * and pass the provided props.
+     */
+    $render =  Inertia::render('example', [
+      'props' => [
+        'heading' => 'Example Heading',
+        'body' => 'Example Body',
       ],
-    ], "/inertia/$path", 'c32b8e4965f418ad16eaebba1d4e960f');
-
-    // return [
-    //   '#theme' => 'inertia_example',
-    //   //'#test_var' => $this->t('Test Value'),
-    // ];
+    ], "/inertia/example", 'some_hash');
+    $render['#attached']['library'][] =  'inertia_examples/app';
+    return $render;
   }
 
 }
